cf55b7444a1a7dc842858c3ce8be3526cb66c83d
Yuriy Tarasenko <maxjeff@mail.ru>
2022-07-17 16:39:40 +0300

    3. Bootstrap, Table [#504840]

---
+++ src/main/java/ru/job4j/dreamjob/model/PostStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "какое-то описание",
+                LocalDateTime.of(2022, 5, 22, 0, 0, 0, 0)));
+        posts.put(2, new Post(2, "Middle Java Job", "какое-то описание",
+                LocalDateTime.of(2022, 1, 31, 0, 0, 0, 0)));
+        posts.put(3, new Post(3, "Senior Java Job", "какое-то описание",
+                LocalDateTime.of(2021, 12, 10, 0, 0, 0, 0)));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
