6aa195738c6ec306fbb6debe5a8bc3421fdfa600
Yuriy Tarasenko <maxjeff@mail.ru>
2022-07-18 10:29:02 +0300

    4. Thymeleaf, Циклы. [#504841 #323053]

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -61,11 +61,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(name, post.name) && Objects.equals(description, post.description) && Objects.equals(created, post.created);
+        return id == post.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, created);
+        return Objects.hash(id);
     }
 }
